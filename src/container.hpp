b401439c9c57878b062eb4fd4561e78fbaffca1c
mastamysta <benjamin-read@hotmail.co.uk>
2024-10-20 21:05:45 +0100

    Initial implementation of container and iterator.

--- src/container.hpp
+++ src/container.hpp
@@ -1,8 +1,114 @@
 #pragma once
 
+#include <algorithm>
+#include <initializer_list>
+
+template <typename T>
+class BreadbinIterator
+{
+public:
+
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = unsigned int;
+    using value_type = T;
+    using pointer = T*;
+    using reference = T&;
+
+    explicit BreadbinIterator() {}
+    explicit BreadbinIterator(pointer start_addr) : start_addr(start_addr) {}
+
+    explicit BreadbinIterator(const BreadbinIterator& other) : ind(other.ind) {}
+
+    reference operator*() const
+    {
+        return *(start_addr + ind);
+    }
+
+    pointer operator->() const
+    {
+        return start_addr + ind;
+    }
+
+    BreadbinIterator& operator++()
+    {
+        ind++;
+        return *this;
+    }
+
+    BreadbinIterator operator++(T)
+    {
+        BreadbinIterator tmp = *this;
+        ++(this);
+        return tmp;
+    }
+
+    friend bool operator==(const BreadbinIterator& lhs, const BreadbinIterator& rhs)
+    {
+        return lhs.ind == rhs.ind;
+    }
+
+    friend bool operator!=(const BreadbinIterator& lhs, const BreadbinIterator& rhs)
+    {
+        return lhs.ind != rhs.ind;
+    }
+
+private:
+
+    difference_type ind;
+    pointer start_addr;
+
+};
+
 template <typename ValueType>
 class Breadbin
 {
 public:
     using value_type = ValueType;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using difference_type = int;
+    using size_type = unsigned int;
+    using iterator = BreadbinIterator<value_type>;
+    using const_iterator = const BreadbinIterator<value_type>;
+
+    static constexpr size_type capacity()
+    {
+        return 10;
+    }
+
+    // TODO: Maybe nodiscard??
+    explicit Breadbin() { /* Do nothing! */ }
+
+    explicit Breadbin(const Breadbin& other)
+    {
+        std::copy(other.data, other.data + other.capacity(), data);
+    }
+
+    explicit Breadbin(std::initializer_list<value_type> vals)
+    {
+        std::copy(std::begin(vals), std::end(vals), data);
+    }
+
+    Breadbin operator=(Breadbin other)
+    {
+        std::copy(other.data, other.data + other.capacity(), data);
+    }
+
+    Breadbin operator=(Breadbin&& other)
+    {
+        std::copy(other.data, other.data + other.capacity(), data);
+    }
+
+    iterator begin()
+    {
+        return BreadbinIterator<value_type>(data);
+    }
+
+    iterator end()
+    {
+        return BreadbinIterator<value_type>(data + capacity());
+    }
+
+private:
+    value_type data[10]; // The breadbin always contains 10 elements
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+
 #include "container.hpp"
 
 int main()
 {
     Breadbin<float> bin;
+
+    Breadbin<int> b2 ({ 0, 1, 2, 3});
+
+    for (const auto& val: b2)
+        std::cout << val << "\n";
 
     return 0;
 }
